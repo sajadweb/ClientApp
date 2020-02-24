956c8edd8f3c68baf6a8ab99dc55fac102de7199
sajad web <sajadweb7@gmail.com>
2020-02-25 01:08:54 +0330

    first commit

---
+++ babel.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+      },
+    ],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    'styled-components',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    ['import', { libraryName: 'antd', style: true }, 'ant'],
+  ],
+  env: {
+    production: {
+      only: ['client', 'server'],
+      plugins: [
+        'lodash',
+        'transform-react-remove-prop-types',
+        '@babel/plugin-transform-react-inline-elements',
+        '@babel/plugin-transform-react-constant-elements',
+        ['import', { libraryName: 'antd', style: true }, 'ant'],
+      ],
+    },
+    test: {
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        'dynamic-import-node',
+      ],
+    },
+  },
+};
